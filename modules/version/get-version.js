e2a4fae1df03afad9f131aa4c601385b056d1b2d
Emanuele DelBono <emanuele@codiceplastico.com>
2016-11-23 15:50:37 +0100

    removed version route

--- modules/version/get-version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-module.exports = function(req, res) {
-	fs.readFile('./package.json', 'utf8', function(err, data) {
-	  	const info = JSON.parse(data)
-	  	res.json({name: info.name, version: info.version})
-	})
-}
--- modules/version/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/version', require('./get-version'))
-
-module.exports = router
